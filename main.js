1f0350e3e4c9c691b83e394b5e4d6beb6ebbebba
DhevaMargaPutra <dhevamp57@gmail.com>
2019-01-09 05:29:12 +0700

    kdua

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ const inputFld = document.getElementById("in");
 
 var jadwalArr = [];
 
-jadwalArr = 
+if (JSON.parse(localStorage.getItem ("jList") != null)) {
+    jadwalArr = JSON.parse(localStorage.getItem ("jList"));
+    render ();
+}
+
 
 masukanBtn.addEventListener ("click", function() {
     if (inputFld.value != "") {
         jadwalArr.push(inputFld.value);
+        localStorage.setItem ("jList", JSON.stringify(jadwalArr));
         render ();
-        localStorage.setItem ("jList", jadwalArr);
         inputFld.value = "";
     } else {
         alert ("Masukan sebuah jadwal!");
@@ -22,11 +26,14 @@ function render () {
     var i = 0;
     jadwalLst.innerHTML = "";
     while (i < jadwalArr.length) {
-        jadwalLst.innerHTML += '<li>' + jadwalArr[i] + '<button>X</button></li>';
+        jadwalLst.innerHTML += '<li>' + jadwalArr[i] + '<button onclick="delList(' + i + ')">X</button></li>';
         i++;
     }
 }
 
-function getListData () {
-    
+function delList (index) {
+    console.log (index);
+    jadwalArr.slice (index, 1);
+    console.log (jadwalArr);
+    render ();
 }
