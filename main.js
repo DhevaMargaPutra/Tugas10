d6687660af2d288e96b460e7e5fcf2077c848db7
DhevaMargaPutra <dhevamp57@gmail.com>
2019-01-10 18:00:37 +0700

    pertamax_tugas10

--- main.js
+++ main.js
@@ -2,66 +2,51 @@ const jadwalLst = document.getElementById("lst");
 const masukanBtn = document.getElementById("sub");
 const inputFld = document.getElementById("in");
 
-var jadwalArr = [];
-var checkedArr = [];
-
-if (JSON.parse(localStorage.getItem ("jList") != null)) {
-    jadwalArr = JSON.parse(localStorage.getItem ("jList"));
-    render ();
-}
-
-if (JSON.parse(localStorage.getItem ("cList") != null)) {
-    checkedArr = JSON.parse(localStorage.getItem ("cList"));
-    render ();
-}
-checkedArr = JSON.parse(localStorage.getItem ("cList"));
-    render ();
+var todos = [];
+todos = JSON.parse (localStorage.getItem ("todos"));
+render ();
 
 masukanBtn.addEventListener ("click", function() {
     if (inputFld.value != "") {
-        jadwalArr.push(inputFld.value);
-        checkedArr.push("0");
+        var todo = {
+            name : inputFld.value,
+            status : false
+        };
+        todos.push (todo);
         render ();
         inputFld.value = "";
     } else {
         alert ("Masukan sebuah jadwal!");
     }
-    console.log (localStorage.getItem("jList"));
-    console.log (localStorage.getItem("cList"));
 });
 
 function render () {
     var i = 0;
     jadwalLst.innerHTML = "";
-    var isChecked = "";
-    while (i < jadwalArr.length) {
-        if (checkedArr[i] == "1") {
-            isChecked = "checked";
+    while (i < todos.length) {
+        if (todos[i].status == true) {
+            jadwalLst.innerHTML += '<li class="list-group-item"><div class="checkbox-item"><input type="checkbox" checked onchange="saveChecked(this, '+i+')"></div><div class="text-item">'+todos[i].name+'</div><div class="delete-item"><button type="button" class="btn btn-outline-danger" onclick="delList('+i+')">X</button></div></li>';
         } else {
-            isChecked = "";
+            jadwalLst.innerHTML += '<li class="list-group-item"><div class="checkbox-item"><input type="checkbox" onchange="saveChecked(this, '+i+')"></div><div class="text-item">'+todos[i].name+'</div><div class="delete-item"><button type="button" class="btn btn-outline-danger" onclick="delList('+i+')">X</button></div></li>';
         }
-        jadwalLst.innerHTML += '<li class="list-group-item"><div class="checkbox-item"><input type="checkbox" '+isChecked+' onchange="saveChecked(this, '+i+')"></div><div class="text-item">'+jadwalArr[i]+'</div><div class="delete-item"><button type="button" class="btn btn-outline-danger" onclick="delList('+i+')">X</button></div></li>';
         i++;
     }
-    localStorage.setItem ("jList", JSON.stringify(jadwalArr));
-    localStorage.setItem ("cList", JSON.stringify(checkedArr));
+    localStorage.setItem ("todos", JSON.stringify (todos))
 }
 
 function saveChecked (checkbox, index) {
     if (checkbox.checked == true) {
         console.log ("checked", index);
-        checkedArr[index] = "1";
+        todos[index].status = true;
         render ();
     } else {
         console.log ("dischecked", index);
-        checkedArr[index] = "0";
+        todos[index].status = false;
         render ();
     }
 }
 
 function delList (index) {
-    console.log (index);
-    jadwalArr.splice (index, 1);
-    console.log (jadwalArr);
+    todos.splice (index, 1);
     render ();
 }
